b3f0b5c61f139ff5f009ada27ce71013f259cad1
Donald (Donnie) Palahnuk Jr <dp555@pm.me>
2024-11-09 13:19:43 +0700

    new sep version

---
+++ hw_test1a.py
@@ -0,0 +1,19 @@
+
+
+import random
+import time
+
+greetings = ["Hell0, World!", "Hola, Mundo!", "Bonjour, Monde!", "Ciao, Mondo!", "Hallo, Welt!", "こんにちは、世界！", "안녕하세요, 세계!"]
+
+# Surprise each time with a random greeting
+greeting = random.choice(greetings)
+
+# Get fancy with slow typing effect
+for char in greeting:
+    print(char, end='', flush=True)
+    time.sleep(0.1)
+
+# Just in case it wasn't clear...
+print("\nDid you get that? It's a 'HELL0 WORLD' in style! 😎")
+
+
